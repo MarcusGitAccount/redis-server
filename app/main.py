d11c20b5aad3755115614f0cbcc1b38519a35585
Marcus Pop <marcuspop@Marcuss-MacBook-Pro.local>
2024-03-27 23:40:13 +0200

    CLI tests (cli-test-1711575613892)

--- app/main.py
+++ app/main.py
@@ -6,7 +6,15 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_socket, _ = server_socket.accept() # wait for client
+
+    while True:
+        request: bytes = client_socket.recv(512)
+        data: str = request.decode()
+
+        if "ping" in data.lower():
+            client_socket.send("+PONG\r\n".encode())
+            break
 
 
 if __name__ == "__main__":
