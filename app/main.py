6b12e33a90785c4c0762608ef6caa32922038ace
Marcus Pop <marcuspop@Marcuss-MacBook-Pro.local>
2024-03-31 19:29:12 +0300

    CLI tests (cli-test-1711902552361)

--- app/main.py
+++ app/main.py
@@ -66,7 +66,9 @@ def unix_timestamp() -> int:
 
 def serialize_dataclass(instance) -> list[str]:
     data_dict = asdict(instance)
-    return [f"{key}:{value}" for key, value in data_dict.items()]
+    return [
+        f"{key}:{value}" for key, value in data_dict.items() if not key.startswith("__")
+    ]
 
 
 def random_str(n: int = 40) -> str:
@@ -108,88 +110,140 @@ database: dict[str, ValueItem] = dict()
 replica_sockets: list[socket.socket] = []
 
 
-def handle_client(
-    client_socket: socket, client_address: str, replication_info: ReplicationInfo
-) -> None:
-    print(f"New connection: {client_address}")
+def handle_ping():
+    return encode_resp("PONG")
 
-    keep_client_socket_open: bool = False
+
+def handle_echo(data_decoded):
+    return encode_resp(data_decoded[1])
+
+
+def handle_set(
+    data_decoded,
+    client_socket,
+    client_address,
+    replica_sockets,
+    database_lock,
+    database,
+    replication_info,
+    timestamp,
+    data,
+):
+    with database_lock:
+        for replica_socket in replica_sockets:
+            replicate([data], replica_socket)
+
+        key = data_decoded[1]
+        value = data_decoded[2]
+        expiry_timestamp = MAX_32BIT_TIMESTAMP
+
+        if len(data_decoded) == 5 and "px" == data_decoded[3]:
+            expiry_timestamp = int(data_decoded[4]) + timestamp
+
+        database[key] = ValueItem(value, expiry_timestamp)
+
+        return encode_resp("OK")
+
+
+def handle_get(data_decoded, database_lock, database, timestamp):
+    with database_lock:
+        key = data_decoded[1]
+        value_item = database.get(key, not_found)
+        if (
+            value_item.expiry_timestamp is not None
+            and timestamp >= value_item.expiry_timestamp
+        ):
+            value_item = not_found
+            del database[key]
+
+        return encode_resp(value_item.value)
+
+
+def handle_info(replication_info):
+    return encode_resp("\n".join(serialize_dataclass(replication_info)))
+
+
+def handle_replconf():
+    return encode_resp("OK")
+
+
+def handle_psync(data_decoded, client_address, client_socket, replica_sockets):
+    replication_id = data_decoded[1]
+    if replication_id == "?":
+        new_replication_id = random_str(n=40)
+        response = f"FULLRESYNC {new_replication_id} 0"
+        rdb_bytes = bytes.fromhex(EMPTY_RDB)
+        replica_sockets.append(client_socket)
+        return [
+            encode_resp(response),
+            f"${len(rdb_bytes)}\r\n".encode("utf-8") + rdb_bytes,
+        ]
+
+    return None
+
+
+def handle_client(
+    client_socket: socket.socket,
+    client_address: str,
+    replication_info: ReplicationInfo,
+    send_response_back: bool = True,
+    buffer_size: int = 1024,
+):
+    print(f"New connection: {client_address}")
+    keep_client_socket_open = False
 
     while True:
         try:
-            # Receive data from client
-            request = client_socket.recv(128)
-            timestamp: int = unix_timestamp()  # client sent data, get timestamp
-
+            request = client_socket.recv(buffer_size)
             if not request:
-                # No more data from client, close connection
                 print(f"Connection closed by client: {client_address}")
                 break
 
-            data: str = request.decode()
+            timestamp = unix_timestamp()
+            data = request.decode()
             data_decoded, _ = decode_resp(data)
-            command: str = data_decoded[0].lower()
+            command = data_decoded[0].lower()
             print(f"Received from {client_address}: {data_decoded} at {timestamp}")
+            print(f"Raw data: {data.replace("\r\n", "\\r\\n")}")
 
-            multipart_response: list[str] = [encode_resp(None)]
-            if "ping" == command:
-                multipart_response = [encode_resp("PONG")]
-            elif "echo" == command:
-                multipart_response = [encode_resp(data_decoded[1])]
-            elif "set" == command:
-                for replica_socket in replica_sockets:
-                    replicate([data], replica_socket)
+            if command == "ping":
+                response = handle_ping()
+            elif command == "echo":
+                response = handle_echo(data_decoded)
+            elif command == "set":
+                response = handle_set(
+                    data_decoded,
+                    client_socket,
+                    client_address,
+                    replica_sockets,
+                    database_lock,
+                    database,
+                    replication_info,
+                    timestamp,
+                    data,
+                )
+            elif command == "get":
+                response = handle_get(data_decoded, database_lock, database, timestamp)
+            elif command == "info":
+                response = handle_info(replication_info)
+            elif command == "replconf":
+                response = handle_replconf()
+            elif command == "psync":
+                response = handle_psync(
+                    data_decoded, client_address, client_socket, replica_sockets
+                )
+                keep_client_socket_open = True
 
-                key: str = data_decoded[1]
-                value: object = data_decoded[2]
-                expiry_timestamp: int = MAX_32BIT_TIMESTAMP
-
-                if len(data_decoded) == 5 and "px" == data_decoded[3]:
-                    expiry_timestamp = int(data_decoded[4]) + timestamp
-
-                with database_lock:
-                    database[key] = ValueItem(value, expiry_timestamp)
-
-                multipart_response = [encode_resp("OK")]
-            elif "get" == command:
-                key: str = data_decoded[1]
-                with database_lock:
-                    value_item: ValueItem = database.get(key, not_found)
-                    print(value_item)
-                    if (
-                        value_item.expiry_timestamp is not None
-                        and timestamp >= value_item.expiry_timestamp
-                    ):
-                        value_item = not_found
-                        del database[key]
-
-                multipart_response = [encode_resp(value_item.value)]
-            elif "info" == command:
-                multipart_response = [
-                    encode_resp("\n".join(serialize_dataclass(replication_info)))
-                ]
-            elif "replconf" == command:
-                multipart_response = [encode_resp("OK")]
-            elif "psync" == command:
-                replication_id: str = data_decoded[1]
-                offset: int = int(data_decoded[2], 10)
-                if replication_id == "?":
-                    new_replication_id: str = random_str(n=40)
-                    response: str = f"FULLRESYNC {new_replication_id} 0"
-                    rdb_bytes: bytes = bytes.fromhex(EMPTY_RDB)
-                    multipart_response = [
-                        encode_resp(response),
-                        f"${len(rdb_bytes)}\r\n".encode("utf-8") + rdb_bytes,
-                    ]
-
-                    print(f"Adding replica {client_address}")
-                    replica_sockets.append(client_socket)
-                    keep_client_socket_open = True
-
-            for part in multipart_response:
-                if not isinstance(part, bytes):
-                    part = part.encode("utf-8")
-                client_socket.send(part)
+            if not send_response_back:
+                continue
+            if response:
+                if isinstance(response, list):
+                    for part in response:
+                        client_socket.send(
+                            part if isinstance(part, bytes) else part.encode("utf-8")
+                        )
+                else:
+                    client_socket.send(response.encode("utf-8"))
 
         except Exception as e:
             print(f"Error with {client_address}: {e}")
@@ -199,18 +253,40 @@ def handle_client(
         client_socket.close()
 
 
-def send_messages_to_master(messages: list[str], server_info: tuple[str, int]) -> str:
+def handle_master_conn(
+    master_socket: socket.socket,
+) -> None:
     """
-    Assumess message is RESP encoded
+    Assumess message is RESP encoded. 1-2 messages are expected after
     """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:  # tcp over ipv4
-        sock.connect(server_info)
-        for message in messages:
-            pprint(f"Sending message to master: {message}")
-            sock.sendall(message.encode("utf-8"))
-            response: bytes = sock.recv(128)
-            pprint(f"Master responded with {response.decode(errors='ignore')}")
-    return response
+    # Continue listening for messages from master
+    while True:
+        incoming_message = master_socket.recv(128)
+
+        if not incoming_message:
+            print("Connection closed by master...")
+            break
+
+        timestamp = unix_timestamp()
+        data = incoming_message.decode(errors="ignore")
+        data_decoded, _ = decode_resp(data)
+        command = data_decoded[0].lower()
+
+        pprint(f"Received from master replication command: {data_decoded}")
+
+        if command == "set":
+            handle_set(
+                data_decoded,
+                None,
+                None,
+                replica_sockets,
+                database_lock,
+                database,
+                None,
+                timestamp,
+                data,
+            )
+    master_socket.close()
 
 
 def replicate(messages: list[str], sock: socket.socket) -> None:
@@ -220,26 +296,6 @@ def replicate(messages: list[str], sock: socket.socket) -> None:
     for message in messages:
         pprint(f"Replicating {message} to {sock}")
         sock.sendall(message.encode("utf-8"))
-
-
-def run_slave_configuration(
-    master_host: str,
-    master_port: int,
-    own_port: int,
-) -> None:
-    master_info: tuple[str, int] = (master_host, master_port)
-
-    print("Server is in [SLAVE] mode, will try to connect to master...")
-    print(f"Pinging master over: {master_info}")
-    send_messages_to_master(
-        [
-            encode_resp(["PING"]),
-            encode_resp(["REPLCONF", "listening-port", str(own_port)]),
-            encode_resp(["REPLCONF", "capa", "psync2"]),
-            encode_resp(["PSYNC", "?", "-1"]),
-        ],
-        master_info,
-    )
 
 
 def start_redis_server():
@@ -256,6 +312,8 @@ def start_redis_server():
         type=str,
     )
     args = parser.parse_args()
+    master_info: tuple[str, int] = None
+    master_thread: threading.Thread = None
 
     role: str = MASTER_REPLICATION
     if args.replicaof is not None:
@@ -265,22 +323,57 @@ def start_redis_server():
             port = int(port_str)
         except ValueError:
             parser.error("PORT must be an integer")
-        run_slave_configuration(host, port, args.port)
 
-    replication_info = ReplicationInfo(role=role)
+        print("Server is in [SLAVE] mode, will try to connect to master...")
+        master_socket = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM
+        )  # tcp over ipv4
+        master_info = (host, port)
+        master_socket.connect(master_info)
+
+        print(f"Pinging master over: {master_info}")
+
+        master_socket.sendall(encode_resp(["PING"]).encode())
+        assert master_socket.recv(128).decode().strip() == "+PONG"
+
+        master_socket.sendall(
+            encode_resp(["REPLCONF", "listening-port", str(args.port)]).encode()
+        )
+        assert master_socket.recv(1024).decode().strip() == "+OK"
+
+        master_socket.sendall(encode_resp(["REPLCONF", "capa", "psync2"]).encode())
+        assert master_socket.recv(1024).decode().strip() == "+OK"
+
+        master_socket.sendall(encode_resp(["PSYNC", "?", "-1"]).encode())
+        psync_msg = master_socket.recv(56).decode()
+        rdb_msg = master_socket.recv(2048)
+
+        master_thread = threading.Thread(
+            target=handle_client,
+            args=(master_socket, master_info, ReplicationInfo(role="slave"), False, 32),
+        )
+        master_thread.start()
+
+        print("Waiting a bit to accept new client connections...")
+        time.sleep(1.5)
 
     server_socket = socket.create_server(("localhost", args.port), reuse_port=True)
     server_socket.listen()
     threads = []
 
     print(f"Server is listening for connections on port {args.port}...")
-
     try:
         while True:
             client_socket, client_address = server_socket.accept()  # wait for client
             client_thread = threading.Thread(
                 target=handle_client,
-                args=(client_socket, client_address, replication_info),
+                args=(
+                    client_socket,
+                    client_address,
+                    ReplicationInfo(role=role),
+                    True,
+                    1024,
+                ),
             )
             client_thread.daemon = True  # daemon threads are killed automatically when the main program exits
             client_thread.start()
@@ -289,25 +382,10 @@ def start_redis_server():
         server_socket.close()
         for thread in threads:
             thread.join()  # Wait for all client threads to finish
+        if master_thread is not None:
+            master_thread.join()
         print("Server has been gracefully shutdown.")
 
 
 if __name__ == "__main__":
     start_redis_server()
-    # print(decode_resp("*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"))
-    # a = bytes.fromhex(EMPTY_RDB)
-    # print(len(a))
-    # print(decode_resp('*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n'))
-    # print(decode_resp("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"))
-    # print(parse_resp(":-123\r\n"))
-    # resp: str ='\r\n'.join(['*2', '$4', 'echo', '$5', 'mango', ''])
-    # print(resp)
-    # print(parse_resp(resp))
-    # print(parse_resp('*1\r\n$4\r\nping\r\n'))
-    # print(encode_resp(None))
-
-    """
-                    1711644520407554
-    expiry_timestamp=1711644520407654
-                    1711644520410214
-    """
